fe1a10e7d2bbfb1eac999d54e8c33a33f414ed4f
Miles Steele <miles@milessteele.com>
2014-12-18 11:29:00 -0500

    add cli interface

--- bee.py
+++ bee.py
@@ -1,6 +1,26 @@
+"""
+Python Bee
+
+Usage:
+  bee.py list
+  bee.py start <player> [<problem>]
+  bee.py test <player> <problem>
+  bee.py test <file>
+  bee.py test (-l | --last)
+  bee.py wipe --all
+
+Options:
+  -h --help  Show this screen.
+  start      List all available problems.
+  start      Create a template for a contestant entry.
+  test       Test a contestant entry.
+  wipe       Delete all existing solutions
+"""
+
 import re
 from collections import namedtuple
 from pprint import pprint
+from docopt import docopt
 
 PROBLEMS_FILE = "problems.txt"
 
@@ -36,8 +56,30 @@ def make_entry_file(problem, path):
         comment = lambda line: "# {}\n".format(line)
         f.writelines(map(comment, problem.text))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    print arguments, '\n'
+
     problems = load_problems()
-    p = problems.values()[0]
-    make_entry_file(p, "entry_{}.py".format(p.name))
-    print("bee.py implementation not finished")
+
+    if arguments['list']:
+        print "Problems loaded from: {}".format(PROBLEMS_FILE)
+        for probname in problems:
+            print probname
+    elif arguments['start']:
+        player = arguments['<player>']
+        probname = arguments['<problem>']
+        if not probname:
+            raise NotImplementedError("'Start' on randomized problem not implemented")
+        p = problems.get(probname)
+        if not p:
+            print "No such problem: '{}'".format(probname)
+            raise SystemExit
+
+        entryfile = "entries/{}_{}.py".format(p.name, player)
+        make_entry_file(p, entryfile)
+        print "Created entry for '{}': {}".format(p.name, entryfile)
+    elif arguments['test']:
+        raise NotImplementedError("'Test' not implemented")
+    elif arguments['wipe']:
+        raise NotImplementedError("'Wipe' not implemented")
