acc5b590ee41c75b23cb454cc7f063b8b961ad41
Miles Steele <miles@milessteele.com>
2014-12-18 12:07:08 -0500

    update palindrome test

--- tests/palindrome.py
+++ tests/palindrome.py
@@ -1,3 +1,4 @@
 def test(fn):
+    fn = entry.palindrome
     assert fn("foo") == False, "foo is not a palindrome"
     assert fn("rewarder redrawer") == False, "'rewarder redrawer' is a palindrome"
